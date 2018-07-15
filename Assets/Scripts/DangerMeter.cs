cc901056eecce1580070fd8a9102d9c6da2a9a80
crassSandwich <christopherjanidlo@gmail.com>
2018-07-15 14:11:07 -0400

    Danger Meter

---
+++ Assets/Scripts/DangerMeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DangerMeter : MonoBehaviour {
+
+	public float MaxDangerDistance, SafeDistance;
+
+	Image meter;
+
+	void Start () {
+		meter = GetComponent<Image>();
+	}
+	
+	void Update () {
+		float dist = EvilPlane.Instance.Distance;
+		meter.fillAmount = (dist - SafeDistance) / (MaxDangerDistance - SafeDistance);
+	}
+
+}
---
+++ Assets/Scripts/EvilPlane.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilPlane : MonoBehaviour {
+
+	public static EvilPlane Instance;
+
+	public Transform Target;
+	public float Distance { get; private set; }
+
+	Plane plane;
+
+	void Start () {
+		Instance = this;
+	}
+	
+	void Update () {
+		plane = new Plane(transform.forward, transform.position);
+		Distance = plane.GetDistanceToPoint(Target.position);
+	}
+
+}
