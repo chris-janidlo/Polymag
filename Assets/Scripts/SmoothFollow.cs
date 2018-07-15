5cc11d6a538c312eb51aa1495f9d95a8023d9d50
crassSandwich <christopherjanidlo@gmail.com>
2018-07-15 01:38:13 -0400

    This is illegal

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour {
+
+	public Transform Target;
+	public float MoveDelay, RotationFactor;
+
+	Vector3 velocity = Vector3.zero;
+	
+	void Update () {
+		transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref velocity, MoveDelay);
+		transform.rotation = Quaternion.Slerp(transform.rotation, Target.rotation, RotationFactor);
+	}
+
+}
