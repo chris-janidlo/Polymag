1907c930a5bee2efd03a8d937ae35f0964ec4e4b
crassSandwich <christopherjanidlo@gmail.com>
2018-09-12 00:02:32 -0400

    Remove extraneous variable

--- Assets/Scripts/LineDetection.cs
+++ Assets/Scripts/LineDetection.cs
@@ -7,7 +7,6 @@ public class LineDetection : MonoBehaviour {
 	[Tooltip("Graph where x = shortest distance between the player and the track center, and y = the associated drag.")]
 	public AnimationCurve DragByDistanceToCurve;
 
-	int layerMask = 1 << 8;
 	Rigidbody rb;
 
 	void Start () {
