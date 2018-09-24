c91c2a6cddff67216891a9daf0190fd14cf7e6fa
crassSandwich <christopherjanidlo@gmail.com>
2018-09-24 00:42:32 -0400

    Difficulty decreases
    - Drag formula tweaks
    - Bigger crystal hitbox
    - Larger track (twice as big!)
    - Fix for line/particle renderers overlapping weirdly
    - Other visual changes intended to increase discernibility

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DragController : MonoBehaviour {
 
-	[Tooltip("Graph where x = shortest distance between the player and the track center, and y = the associated drag.")]
+	[Tooltip("Graph where x = player distance from track center as percentage of track width, and y = the associated drag.")]
 	public AnimationCurve DragByDistanceToCurve;
 
 	Rigidbody rb;
@@ -14,8 +14,9 @@ public class DragController : MonoBehaviour {
 	}
 
 	void Update () {
-		float dist = Track.Instance.DistanceFromCurve(transform.position);
-		rb.drag = DragByDistanceToCurve.Evaluate(dist);
+		float percent = Track.Instance.DistanceFromCurve(transform.position) / Track.Instance.LineDistance;
+		Debug.Log(percent);
+		rb.drag = DragByDistanceToCurve.Evaluate(percent);
 	}
 
 }
